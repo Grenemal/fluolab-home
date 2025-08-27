b7d05ef555fad2615e35c4e20dfd8c0e1ac69bd4
Grenemal <65065585+Grenemal@users.noreply.github.com>
2025-08-27 14:34:14 +0800

    添加搜索框

---
+++ src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<!-- filepath: c:\Users\Jone_\Documents\Github\fluolab-home\src\views\HomePage.vue -->
+<template>
+  <div>
+    <!-- Other homepage content -->
+    <SearchBox />
+  </div>
+</template>
+
+<script>
+import SearchBox from "@/components/SearchBox.vue";
+
+export default {
+  components: {
+    SearchBox,
+  },
+};
+</script>
